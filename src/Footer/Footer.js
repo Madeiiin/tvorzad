5585811f0ee5dfbb86844b10c2704cfedb800f90
Madeiin <67866151+Madeiin@users.noreply.github.com>
2022-05-17 17:26:50 +0400

    Initial commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <>
+        <footer>
+            <div className="footer-info">
+                <p>Разработал:<br/>
+                    Васильев А.Р.</p>
+            </div>
+            <div className="footer-info">
+
+            </div>
+        </footer>
+        </>
+    )
+}
+export default Footer
